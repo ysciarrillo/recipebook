26f73ed25d488a5367d11aa6d87097a131e0d827
ysciarrillo <lazyparaffin@gmail.com>
2018-08-20 13:24:35 -0400

    Initial commit

---
+++ server/mailer.js
@@ -0,0 +1,3 @@
+Meteor.startup( function() {
+  process.env.MAIL_URL = "smtp://postmaster%40sandboxfddfc3f31a0b4aa6a0a9f82587534899.mailgun.org:826c403338e3fee366d8344955246dbd@smtp.mailgun.org:587";
+});
